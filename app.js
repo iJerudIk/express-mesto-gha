4a0d92f3a697bf0d354cb0f4f863dbb0e7462bdc
iJerudIk <iJerudIk@gmail.com>
2022-06-13 18:48:06 +0300

    Express-Mesto: modified 6

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const app = express(); // Создание приложения
 mongoose.connect('mongodb://localhost:27017/mestodb'); // Подключение к БД
 
 app.use(bodyParser.json());
-app.use('/', (req, res) => {
-  res.send({ message: 'Страница' });
-});
 app.use((req, res, next) => {
   req.user = { _id: '62a62a93142d57b1965e78c8' };
   next();
 });
 app.use('/', userRoutes);
 app.use('/', cardRoutes);
+app.use('/', (req, res) => {
+  res.send({ message: 'Страница' });
+});
 
 // Запуск сервера
 app.listen(3000, () => {
